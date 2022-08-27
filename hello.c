b45ebfbe1d6010f2d6815966d093dbedd5bb5e7c
jhartley3 <jhartley3@huskers.unl.edu>
2022-08-27 20:04:52 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Josephine Hartley
+ *         jhartley3@huskers.unl.edu
+ * Date: 2022/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int 
+main(int argc, char **argv) {
+
+  printf("Josephine Hartley, Computer Engineering\n");
+
+  return 0;
+}
